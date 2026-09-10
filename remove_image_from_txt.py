b513ef340be6308e1e6cf9dce02c3113d5d4bed3
rebecabg <rebecabrgarcia@gmail.com>
2026-09-09 21:13:07 -0300

    remove txt e imagens sequenciais de placa r6a pra evitar overfitting

--- remove_image_from_txt.py
+++ remove_image_from_txt.py
@@ -7,12 +7,12 @@ from pathlib import Path
 
 # Pasta onde estão os arquivos TXT
 PASTA_TXT = Path(
-    "placas_organizadas/imagens_ja_feitas_train/placa_pedestre"
+    "placas_organizadas/imagens_ja_feitas_train/proibido_estacionar"
 )
 
 # Pasta onde estão as imagens
 PASTA_IMAGENS = Path(
-    "placas_organizadas/train/A-32b-_Passagem_sinalizada_de_pedestres"
+    "placas_organizadas/train/R-6a-_Proibido_estacionar"
 )
 
 # Extensões consideradas como imagens
