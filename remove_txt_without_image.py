62ef1a8db4bc3957bc9539f7ec64070cf814d806
rebecabg <rebecabrgarcia@gmail.com>
2026-09-11 15:08:15 -0300

    clean

--- remove_txt_without_image.py
+++ remove_txt_without_image.py
@@ -7,12 +7,12 @@ from pathlib import Path
 
 # Pasta onde estão os arquivos TXT
 PASTA_TXT = Path(
-    "placas_organizadas/imagens_ja_feitas_train/r-4a-proibido-virar-esquerda"
+    "placas_organizadas/imagens_ja_feitas_train/a2b-curva-direita"
 )
 
 # Pasta onde estão as imagens
 PASTA_IMAGENS = Path(
-    "placas_organizadas/train/R-4a-_Proibido_virar_a_esquerda"
+    "placas_organizadas/train/A-2b-_Curva_a_direita"
 )
 
 # Extensões consideradas como imagens
